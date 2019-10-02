d7e2676ccd2d2a001be87adf286f428af8452767
tuan-bacoor <huynhtuan@bacoor.co>
2019-10-02 16:21:59 +0700

    all

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test deploy with gh-pages
         </a>
       </header>
     </div>
